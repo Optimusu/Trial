f593288e9bed3d8dbaf76ec54221b280d49fadc7
Hebert Merchan <merchanhebert@gmail.com>
2025-07-10 16:00:33 -0400

    Just created CountriesController, with apiversion... let try first how work.

---
+++ Trial.AppBack/Controllers/CountriesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trial.AppBack.Controllers
+{
+    [Route("api/v{version:apiVersion}/countries")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+    }
+}
