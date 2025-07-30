28cb8fdc972aedaa5cd5e33fc2fb4e266bb70b3c
Hebert Merchan <merchanhebert@gmail.com>
2025-07-30 15:47:41 -0400

    Creacion de Enum para TrialPhase para manejo de las fases del estudio

---
+++ Trial.Domain/Enum/TrialPhase.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trial.Domain.Enum;
+
+public enum TrialPhase
+{
+    [Display(Name = "Phase I")]
+    PhaseI = 1,
+
+    [Display(Name = "Phase II")]
+    PhaseII = 2,
+
+    [Display(Name = "Phase III")]
+    PhaseIII = 3,
+
+    [Display(Name = "Phase IV")]
+    PhaseIV = 4
+}
